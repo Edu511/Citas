956ff23506ab3e0418508f78324aa629b385fa6f
Jose Eduardo <jeduardo180895@gmail.com>
2021-08-06 17:21:42 -0500

    fire fallido

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -2,14 +2,14 @@ import firebase from 'firebase'
 import Axios from 'axios'
 
 const config = {
-  apiKey: 'AIzaSyDtFg8icEWaIN5jD67mz-EmNkI_qD_wMaY',
-  authDomain: 'citas-b8afe.firebaseapp.com',
-  projectId: 'citas-b8afe',
-  storageBucket: 'citas-b8afe.appspot.com',
-  messagingSenderId: '38435234907',
-  appId: '1:38435234907:web:21c8240a7d706a3f5b8b7e',
-  measurementId: 'G-0P8CDE5WZV'
-
+  apiKey: "AIzaSyDtFg8icEWaIN5jD67mz-EmNkI_qD_wMaY",
+  authDomain: "citas-b8afe.firebaseapp.com",
+  databaseURL: "https://citas-b8afe-default-rtdb.firebaseio.com",
+  projectId: "citas-b8afe",
+  storageBucket: "citas-b8afe.appspot.com",
+  messagingSenderId: "38435234907",
+  appId: "1:38435234907:web:21c8240a7d706a3f5b8b7e",
+  measurementId: "G-0P8CDE5WZV"
 }
 
 firebase.initializeApp(config)
