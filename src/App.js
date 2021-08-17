16f144a2373798d5bf759dc9490a5d5b52d23da6
Sthefany Rangel <sthefanyrangel.it@gmail.com>
2021-08-17 12:03:43 -0500

    config - eliminacion de axios de app.js

--- src/App.js
+++ src/App.js
@@ -7,13 +7,9 @@ import Citas from './components/citas/Citas'
 import Edit from './components/citas/Edit'
 import Contact from './components/contact/Contact'
 import Msg from './components/contact/Msg'
-import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.min.css'
 //import { useForm } from 'react-hook-form'
 
-const api = axios.create({
-  baseURL: 'http://localhot:3000/contact/'
-});
 
 
 function App (props) {
@@ -21,10 +17,6 @@ function App (props) {
   // react hooks form    
   // const { register, handleSubmit } = useForm();
   // const onSubmit = data => console.log(data);
-
-  api.get('/').then(res => {
-    alert();
-  })
 
   return (
     <Switch>
